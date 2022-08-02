f565e85e9deee0fb9e7c5d551088938214fb15aa
C0dezinnn <73194980+c0dezinnn@users.noreply.github.com>
2022-08-02 15:35:56 -0300

    Create cheetosmenu.lua

---
+++ cheetosmenu.lua
@@ -0,0 +1,10 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Detecting game...", "DarkTheme")
+local LaunchTab = Window:NewTab("Detecting game...")
+	local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Detecting game...");
+
+-- starving artists 
+if game.PlaceId == 8916037983 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/c0dezinnn/cheetos/main/starving%20artists%20chettos.lua"))()
+end 
