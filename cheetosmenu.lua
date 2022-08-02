a6693e802afbd697efa8761a56d5b44e41a7a219
C0dezinnn <73194980+c0dezinnn@users.noreply.github.com>
2022-08-02 20:37:52 -0300

    Update cheetosmenu.lua

--- cheetosmenu.lua
+++ cheetosmenu.lua
@@ -3,12 +3,23 @@ local loadinggame = loadstring(game:HttpGet("https://raw.githubusercontent.com/c
 print("detecting game...")
 wait(6)
 loadinggame:destroy()
+gamesupported = "notverified"
 -- starving artists 
 if game.PlaceId == 8916037983 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/c0dezinnn/cheetos/main/starving%20artists%20chettos.lua"))()
+	gamesupported = "yes"
 end 	
 -- murder mystry 2
 if game.PlaceId == 142823291 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/c0dezinnn/cheetos/main/mm2.lua"))()
+	gamesupported = "yes"
+end
+
+if gamesupported == "notverified" then
+	gamesupported = "no"
+end
+
+if gamesupported == "no" then
+	print("not supported yet")
 end
 
