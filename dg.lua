036670ea85f32e0fbf9c70ed630bedd0eb80b3bc
C0dezinnn <73194980+c0dezinnn@users.noreply.github.com>
2022-08-02 20:50:18 -0300

    Update dg.lua

--- dg.lua
+++ dg.lua
@@ -23,6 +23,6 @@ TextLabel.BorderColor3 = Color3.fromRGB(27, 42, 53)
 TextLabel.Position = UDim2.new(0.431587189, 0, 0.396508723, 0)
 TextLabel.Size = UDim2.new(0, 200, 0, 50)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Detecting game..."
+TextLabel.Text = "Detecting the game..."
 TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.TextSize = 20.000
