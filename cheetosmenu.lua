03b28a882e4d787ce12569f81477a99496d08afd
C0dezinnn <73194980+c0dezinnn@users.noreply.github.com>
2022-08-02 20:20:45 -0300

    Update cheetosmenu.lua

--- cheetosmenu.lua
+++ cheetosmenu.lua
@@ -1,5 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local loadinggame = loadstring(game:HttpGet("https://raw.githubusercontent.com/c0dezinnn/cheetos/main/dg.lua"))()
 print("detecting game...")
+wait(6)
+loadinggame:destroy()
 -- starving artists 
 if game.PlaceId == 8916037983 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/c0dezinnn/cheetos/main/starving%20artists%20chettos.lua"))()
@@ -8,5 +11,4 @@ end
 if game.PlaceId == 142823291 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/c0dezinnn/cheetos/main/mm2.lua"))()
 end
-
 
